eddbb67f2ea0e66d1de6e1f4ed278ac730e8fad1
kbertie17 <47428023+kbertie17@users.noreply.github.com>
2019-03-19 11:49:12 -0400

    Add files via upload

---
+++ greeting.java
@@ -0,0 +1,35 @@
+package aaaa;
+
+import java.util.Scanner;
+
+public class greeting {
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in); 
+		
+		String userName;
+		String cont;
+		
+		System.out.println("Would you like to start? Y/N");
+		cont = scnr.next(); 
+		
+		while (cont.charAt(0) == 'Y') {
+			System.out.println("Enter name: ");
+			userName = scnr.next(); 
+			
+			if ((userName.charAt(userName.length() - 1) == 'e') || (userName.charAt(userName.length() - 1) == 'a')) {
+				System.out.println("Miss " + userName);
+			}
+			
+			else {
+				System.out.println("Mister " + userName);
+			}
+			
+			System.out.println("Would you like to proceed? Y/N");
+			cont = scnr.next();
+		}
+ 
+		System.out.println("Goodbye.");
+
+	}
+
+}
