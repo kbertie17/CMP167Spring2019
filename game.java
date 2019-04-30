87443f475565c704053ef6331f16af27a1998709
kbertie17 <47428023+kbertie17@users.noreply.github.com>
2019-04-30 11:37:06 -0400

    Update game.java

--- game.java
+++ game.java
@@ -1,5 +1,40 @@
 package life;
 
 public class game {
+  public static void main(String[] args) {
+		Human person1 = new Human(); 
+		
+//		System.out.println("Hunger: " + person1.hunger);
+//		System.out.println("Food: " + person1.food);
+//		
+//		System.out.println("Eating command:");
+//		person1.eat();
+//		System.out.println("Eating command:");
+//		person1.eat();
+//		System.out.println("Eating command:");
+//		person1.eat();
+//		
+//		System.out.println("Hunger: "+ person1.hunger);
+//		System.out.println("Food: " + person1.food);
+//		
+//		System.out.println("Buying food command:");
+//		person1.buyFood();
+//		
+//		System.out.println("Working command");
+//		person1.work();
+//		
+//		System.out.println("Buying food command");
+//		person1.buyFood();
+//		
+//		System.out.println("Working command");
+//		person1.work();
+//		System.out.println("Working command");;
+//		person1.work();
+//		System.out.println("Eating command:");;
+//		person1.eat();
+//		System.out.println("Working command");;
+//		person1.work();
+	}
 
+}
 }
