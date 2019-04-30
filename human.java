81e71335713175b1fbbcb92ace74a57f03392305
kbertie17 <47428023+kbertie17@users.noreply.github.com>
2019-04-30 11:35:35 -0400

    Update human.java

--- human.java
+++ human.java
@@ -1,5 +1,78 @@
 package life;
 
 public class human {
+  
+	public int age;
+	public String name;
+	public double wealth;
+	public boolean hasVehicle;
+	public double hunger;
+	public int food;
+	Vehicle[] vehicles;
+	
+	public Human() {
+		this.age = 0;
+		this.name = "";
+		this.wealth = 0.0;
+		this.hasVehicle = false;
+		this.hunger = 5.0;
+		this.food = 5;
+		this.vehicles = new Vehicle[100];
+	}
+	
+	public int numberOfVehilces() {
+		int total = 0;
+		for (int i = 0; i < vehicles.length; i++) {
+			if (vehicles[i] != null) {
+				total++;
+			}
+			
+			else {
+				break;
+			}
+		}
+		return total;
+	}
+	
+	public void eat() {
+		if (food >= 4.5) {
+			hunger -= 1.0;
+			food -= 1.5;
+			System.out.println("Yummy, I ate all");
+		}
+		
+		else {
+			System.out.println("Not enough food, please buy more");
+		}
+	}
+	
+	public void grow() {
+		age++;
+	}
+	
+	public void work() {
+		if (hunger < 5) {
+			wealth += 10.5;
+			hunger += .5;
+			System.out.println("Whoah, I worked a lot");
+		}
+		
+		else {
+			System.out.println("Too hungry to work");
+		}
+	}
+	
+	public void buyFood() {
+		if (wealth > 5) {
+			wealth -= 4.5;
+			food++;
+			System.out.println("Yoohoo! I bought some food!");
+		}
+		
+		else {
+			System.out.println("Not enough money, please work");
+		}
+	}
 
+}
 }
